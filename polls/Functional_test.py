20d642632f2d700007ac4c5c338e6a5caa952db1
jdpatino10 <jd.patino10@uniandes.edu.co>
2016-03-17 13:00:13 -0500

    Paso 1-Test para probar que el titulo de la página inicial (Rojo)

---
+++ polls/Functional_test.py
@@ -0,0 +1,15 @@
+__author__ = 'asistente'
+from unittest import TestCase
+from selenium import webdriver
+
+class FunctionalTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
