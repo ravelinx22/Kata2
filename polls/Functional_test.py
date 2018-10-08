d3c5a63d62c0457cb3d809b9a35a2b94c400811f
Diego Rodríguez <diegorbaquero@gmail.com>
2018-10-07 21:07:21 -0500

    Paso 2 - registro (verde)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -18,42 +18,42 @@ class FunctionalTest(TestCase):
         self.assertIn('Busco Ayuda', self.browser.title)
 
     def test_registro(self):
-        #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        #self.browser.get('http://localhost:8000')
-        #link = self.browser.find_element_by_id('id_register')
-        #link.click()
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_register')
+        link.click()
 
-        #nombre = self.browser.find_element_by_id('id_nombre')
-        #nombre.send_keys('William')
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('William')
 
-        #apellidos = self.browser.find_element_by_id('id_apellidos')
-        #apellidos.send_keys('Ravelo')
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Ravelo')
 
-        #experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
-        #experiencia.send_keys('5')
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('5')
 
-        #self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
-        #telefono = self.browser.find_element_by_id('id_telefono')
-        #telefono.send_keys('323424579')
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('323424579')
 
-        #correo = self.browser.find_element_by_id('id_correo')
-        #correo.send_keys('wr.ravelo@uniandes.edu.co')
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('wr.ravelo@uniandes.edu.co')
 
-        #imagen = self.browser.find_element_by_id('id_imagen')
-        #imagen.send_keys(BASE_DIR + "/static/test.png")
+        imagen = self.browser.find_element_by_id('id_imagen')
+        imagen.send_keys(BASE_DIR + "/static/test.png")
 
-        #nombreUsuario = self.browser.find_element_by_id('id_username')
-        #nombreUsuario.send_keys('wrravelo22')
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('wrravelo22')
 
-        #clave = self.browser.find_element_by_id('id_password')
-        #clave.send_keys('clave123')
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
 
-        #botonGrabar = self.browser.find_element_by_id('id_grabar')
-        #botonGrabar.click()
-        #self.browser.implicitly_wait(3)
-        #span=self.browser.find_element(By.XPATH, '//span[text()="William Ravelo"]')
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+        self.browser.implicitly_wait(3)
+        span=self.browser.find_element(By.XPATH, '//span[text()="William Ravelo"]')
 
-        #self.assertIn('William Ravelo', span.text)
+        self.assertIn('William Ravelo', span.text)
 
     def test_verDetalle(self):
         self.browser.get('http://localhost:8000')
